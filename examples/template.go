e0461773e5e48326d9aec55b6009c5cb263c72ce
Paul Maki <paul.maki@gmail.com>
2020-03-30 13:48:47 -0400

    Named paramaters on template

--- examples/template.go
+++ examples/template.go
@@ -18,7 +18,7 @@ func main() {
 	filepath.Walk(startDir, walker)
 }
 
-func walker(path string, info os.FileInfo, err error) error {
+func walker(path string, info os.FileInfo, inErr error) (outErr error) {
 	log.Printf("Current path:%s isdir:%t\n", path, info.IsDir())
 	return nil
 }
